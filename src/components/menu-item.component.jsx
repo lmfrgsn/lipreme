f6be09f2a2e4a09aec0201056c61702d26b4041e
YOUR NAME <YOUR@EMAIL.com>
2020-01-04 19:36:16 +0000

    Break into components, styling

---
+++ src/components/menu-item.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, tagline, imageUrl, size }) => (
+  <div className={`menu-item ${size ? size : ""}`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="sub-title">{tagline}</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
