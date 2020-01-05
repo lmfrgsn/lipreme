aad78f83f654beb2dcf9df6a17d9c2256b7c344d
YOUR NAME <YOUR@EMAIL.com>
2020-01-05 11:57:54 +0000

    Use router higher order component, navigation, prettier to use single quotes,spread on props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from "./pages/homepage/homepage.component";
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  return <h1>Hats</h1>;
+};
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop/hats' component={HatsPage} />
+    </Switch>
   );
 }
 
--- src/components/menu-item.component.jsx
+++ src/components/menu-item.component.jsx
@@ -1,19 +1,25 @@
-import React from "react";
-import "./menu-item.styles.scss";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import './menu-item.styles.scss';
 
-const MenuItem = ({ title, tagline, imageUrl, size }) => (
-  <div className={`menu-item ${size ? size : ""}`}>
+const MenuItem = ({ title, imageUrl, match, linkUrl, size, history }) => (
+  <div
+    className={`menu-item ${size ? size : ''}`}
+    onClick={() => {
+      history.push(`${match.url}${linkUrl}`);
+    }}
+  >
     <div
-      className="background-image"
+      className='background-image'
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
     />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="sub-title">{tagline}</span>
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='sub-title'>Shop Now</span>
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
